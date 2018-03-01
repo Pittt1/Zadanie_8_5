fb8b779269f07448208b098ab5745a43745c5550
Piotr Sławek <piotr.slawek@o2.pl>
2018-03-01 14:36:42 +0100

    Add variables in script

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// script.js file
+
+var name = prompt('Enter your name');
+// alert('Hello, ' + name);
+console.log('Hello, ' + name);
+
+var firstSentence = prompt('I like a song that is called "Only you".');
+
+// break for...
+
+/*
+
+  block comment here...
+
+*/
+
+var a = 7,
+	h = 5,
+	triangleArea;
+
+	triangleArea = a * h / 2;
+
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+
+var secondSentence = prompt("It's " + triangleArea)
